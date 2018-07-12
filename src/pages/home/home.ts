847b0d8c2eda2f40c641499ae9cfe1d0cd7c7b5b
txt_27 <txt_27@guest-wireless-upc-1602-10-120-039-189.usc.edu>
2018-07-12 14:32:42 -0700

    finished home page and got the on click function to work

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ProfilePage } from '../profile/profile';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  openPage(){
+    this.navCtrl.push(ProfilePage)
   }
 
 }
